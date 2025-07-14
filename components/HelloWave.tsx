77e3994b6a56b81db5af08172b3af0b0caae1e2e
pepe tellez <mcbelme92@gmail.com>
2025-07-14 17:57:01 -0600

    miprimercomt

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -9,6 +9,7 @@ import Animated, {
 } from 'react-native-reanimated';
 
 import { ThemedText } from '@/components/ThemedText';
+
 
 export function HelloWave() {
   const rotationAnimation = useSharedValue(0);
@@ -26,7 +27,9 @@ export function HelloWave() {
 
   return (
     <Animated.View style={animatedStyle}>
+      <Text>
       <ThemedText style={styles.text}>👋</ThemedText>
+      </Text>
     </Animated.View>
   );
 }
